f34f93eaa64c3926a33d9ca850e74cf790fac332
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2022-09-07 08:15:54 +0200

    env fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,35 +6,35 @@ import { AppRouter } from "pages/api/trpc/[trpc]";
 import { ReactQueryDevtools } from "react-query/devtools";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <ReactQueryDevtools />
-    </>
-  );
+	return (
+		<>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+			<ReactQueryDevtools />
+		</>
+	);
 };
 
 export default withTRPC<AppRouter>({
-  config({ ctx }) {
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
-    return {
-      url,
-      /**
-       * @link https://react-query.tanstack.com/reference/QueryClient
-       */
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
-    };
-  },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
-  ssr: true,
+	config({ ctx }) {
+		/**
+		 * If you want to use SSR, you need to use the server's full URL
+		 * @link https://trpc.io/docs/ssr
+		 */
+		const url = process.env.NEXT_PUBLIC_VERCEL_URL
+			? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+			: "http://localhost:3000/api/trpc";
+		return {
+			url,
+			/**
+			 * @link https://react-query.tanstack.com/reference/QueryClient
+			 */
+			// queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+		};
+	},
+	/**
+	 * @link https://trpc.io/docs/ssr
+	 */
+	ssr: true,
 })(MyApp);
